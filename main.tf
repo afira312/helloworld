1f8b78219542ee6265ecc1d1e445636f366db368
afira312 <46442563+afira312@users.noreply.github.com>
2024-02-14 17:40:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,17 +15,56 @@ resource "random_id" "id" {
 
   byte_length = 4
 }
-resource "kubernetes_namespace" "example" {
+resource "kubernetes_pod_v1" "test" {
   metadata {
-    annotations = {
-      name = "example-annotation"
+    name = "terraform-example"
+  }
+
+  spec {
+    container {
+      image = "nginx:1.21.6"
+      name  = "example"
+
+      env {
+        name  = "environment"
+        value = "test"
+      }
+
+      port {
+        container_port = 80
+      }
+
+      liveness_probe {
+        http_get {
+          path = "/"
+          port = 80
+
+          http_header {
+            name  = "X-Custom-Header"
+            value = "Awesome"
+          }
+        }
+
+        initial_delay_seconds = 3
+        period_seconds        = 3
+      }
     }
 
-    labels = {
-      mylabel = "label-value"
+    dns_config {
+      nameservers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
+      searches    = ["example.com"]
+
+      option {
+        name  = "ndots"
+        value = 1
+      }
+
+      option {
+        name = "use-vc"
+      }
     }
 
-    name = "terraform-example-namespace"
+    dns_policy = "None"
   }
 }
 output "hello_world" {
