faee7c3bc8f676d015d94c6e8648aedfc33cd3cc
afira312 <46442563+afira312@users.noreply.github.com>
2024-02-14 17:34:12 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,19 @@ resource "random_id" "id" {
 
   byte_length = 4
 }
+resource "kubernetes_namespace" "example" {
+  metadata {
+    annotations = {
+      name = "example-annotation"
+    }
 
+    labels = {
+      mylabel = "label-value"
+    }
+
+    name = "terraform-example-namespace"
+  }
+}
 output "hello_world" {
   value = "Hello World, ${var.subject} ${random_id.id.hex}!"
 }
